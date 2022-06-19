4e5da058d42020132d610cd1707061bc50f85ed6
Sahan Perera <sahanyuganthamail@gmail.com>
2022-06-19 11:47:26 +0530

    cors added

--- src/main/java/com/sahan/cohort5/Cohort5Application.java
+++ src/main/java/com/sahan/cohort5/Cohort5Application.java
@@ -2,12 +2,23 @@ package com.sahan.cohort5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Cohort5Application {
+@Configuration
+public class Cohort5Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cohort5Application.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+				.addMapping("/**")
+				.allowedOrigins("http://localhost:3000") // for any URL "*"
+				.allowedOrigins("*"); //"GET", "POST", "PUT", "DELETE"
+	}
 }
