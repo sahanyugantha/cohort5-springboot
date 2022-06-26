811ef62e79326260cb3caa3526e0384dbfb597b8
Sahan Perera <sahanyuganthamail@gmail.com>
2022-06-26 10:31:47 +0530

    Springboot version downgraded to 2.5 for Swagger UI

--- src/main/java/com/sahan/cohort5/Cohort5Application.java
+++ src/main/java/com/sahan/cohort5/Cohort5Application.java
@@ -2,12 +2,20 @@ package com.sahan.cohort5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @Configuration
+@EnableSwagger2
 public class Cohort5Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
@@ -21,4 +29,37 @@ public class Cohort5Application implements WebMvcConfigurer {
 				.allowedOrigins("http://localhost:3000") // for any URL "*"
 				.allowedOrigins("*"); //"GET", "POST", "PUT", "DELETE"
 	}
+
+//	@Bean
+//	public Docket api() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//				.apis(RequestHandlerSelectors.any())
+//				.paths(PathSelectors.any())
+//				.build();
+//	}
+
+//	@Bean
+//	public Docket api() {
+//		// @formatter:off
+//		//Register the controllers to swagger
+//		//Also it is configuring the Swagger Docket
+//		return new Docket(DocumentationType.SWAGGER_2).select()
+//				// .apis(RequestHandlerSelectors.any())
+//				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+//				// .paths(PathSelectors.any())
+//				// .paths(PathSelectors.ant("/swagger2-demo"))
+//				.build();
+//		// @formatter:on
+//	}
+
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("swagger-ui.html")
+//				.addResourceLocations("classpath:/META-INF/resources/");
+//
+//		registry.addResourceHandler("/webjars/**")
+//				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+//	}
+
 }
